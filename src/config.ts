ca3f72ffa53315fb4dcc4833e40cc9c9b9a10d80
Christian Schleifer <chr.schleifer@gmail.com>
2024-07-06 11:06:52 +0200

    Upgrade dependencies

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE: Site = {
   desc: "Personal blog of Christian Schleifer",
   title: "Christian Schleifer",
   lightAndDarkMode: true,
-  postPerPage: 5,
+  postPerPage: 10,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
